e8fb07a430c3cc6fdef2e047a5a3b726a4851445
nanae <desperado.kazune@gmail.com>
2024-08-17 18:25:19 +0900

    Update tag_to_operators_mapper.py

--- tag_to_operators_mapper.py
+++ tag_to_operators_mapper.py
@@ -1,5 +1,6 @@
 def main():
-    load_operator_list('operators_in_recruitment.csv')
+    file_name = 'operators_in_recruitment_with_rarity.csv'
+    operator_list = load_operator_list(file_name)
 
 def load_tag_list(file_name: str) -> list[str]:
     with open(file_name, 'r') as file:
@@ -7,17 +8,17 @@ def load_tag_list(file_name: str) -> list[str]:
         all_tag: list[str] = content.split('\n')
         return all_tag
 
-def load_operator_list(file_name: str) -> list[tuple]:
+def load_operator_list(file_name: str) -> list[tuple[str, list[str], int]]:
     import csv
 
     res = []
+    all_tag = load_tag_list('all_tag.txt')
 
     with open(file_name, mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            res.append((row['名前'],row['募集タグ']))
-
-    print(*res,sep='\n')
+            tags = split_string_by_tags(row['募集タグ'], all_tag)
+            res.append((row['名前'],tags,int(row['レアリティ'])))
 
     return res
 
@@ -27,6 +28,7 @@ def split_string_by_tags(s: str, all_tag: list[str]) -> list[str]:
         match = next(word for word in all_tag if s.startswith(word))
         result.append(match)
         s = s[len(match):]
+    result.sort()
     return result
 
 if __name__ == '__main__':
