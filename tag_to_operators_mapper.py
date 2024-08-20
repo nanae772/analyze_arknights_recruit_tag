9869d4b67ba578e8ff4432d2ce7eef91fb24db5e
nanae <desperado.kazune@gmail.com>
2024-08-20 20:58:48 +0900

    Add docstring

--- tag_to_operators_mapper.py
+++ tag_to_operators_mapper.py
@@ -1,3 +1,7 @@
+'''
+募集タグの組合せから星４以上のオペレーターが確定する組合せを探すモジュール
+'''
+
 from collections import defaultdict
 from typing import NamedTuple
 import logging
@@ -5,12 +9,18 @@ import csv
 
 
 class Operator(NamedTuple):
+    '''
+    オペレーターを表すオブジェクト
+    '''
     name: str
     tags: tuple[str, ...]
     rarity: int
 
 
 def main():
+    '''
+    メイン関数
+    '''
     file_name = 'operators_in_recruitment_with_rarity.csv'
     operator_list: list[Operator] = load_operator_list(file_name)
     tag_to_operators: dict[str, set[Operator]] = invert_operator_list(operator_list)
@@ -24,6 +34,17 @@ def find_rare_operator_tag_combinations(
         tag_list: list[str],
         tag_to_operators: dict[str, set[Operator]]
 ) -> list[dict[tuple[str, ...], list[Operator]]]:
+    '''
+    募集タグの組合せから星４以上のオペレーターが確定する組合せを探す
+
+    Args:
+        tag_list (list[str]): 募集タグのリスト（５つを想定）
+        tag_to_operators (dict[str, set[Operator]]): {募集タグ: そのタグを持つオペレーターの集合}
+
+    Returns:
+        list[dict[tuple[str, ...], list[Operator]]]:
+            募集タグの組合せ→その組合せで出るオペレーターのリスト、のリスト
+    '''
     if len(tag_list) < 5:
         logging.warning(f'募集タグの個数が少ないです: {len(tag_list)}個')
     tag_list.sort()
@@ -48,6 +69,9 @@ def find_rare_operator_tag_combinations(
 
 
 def load_tag_list(file_name: str) -> list[str]:
+    '''
+    全てのタグが書かれたファイルから全てのタグのリストを作成
+    '''
     with open(file_name, 'r', encoding='utf-8') as file:
         content = file.read()
         all_tag: list[str] = content.split('\n')
@@ -55,6 +79,9 @@ def load_tag_list(file_name: str) -> list[str]:
 
 
 def load_operator_list(file_name: str) -> list[Operator]:
+    '''
+    公開求人で手に入るオペレーターのCSVからオペレーターのリストを作成
+    '''
     res = []
     all_tag = load_tag_list('all_tag.txt')
 
@@ -75,6 +102,9 @@ def load_operator_list(file_name: str) -> list[Operator]:
 
 
 def invert_operator_list(operator_list: list[Operator]) -> dict[str, set[Operator]]:
+    '''
+    オペレーターが持つ募集タグのリストから、ある募集タグを持つオペレーターの逆対応を作る
+    '''
     tag_to_operators: dict[str, set[Operator]] = defaultdict(set)
     for op in operator_list:
         for tag in op.tags:
@@ -83,6 +113,9 @@ def invert_operator_list(operator_list: list[Operator]) -> dict[str, set[Operato
 
 
 def split_string_by_tags(s: str, all_tag: list[str]) -> list[str]:
+    '''
+    空白無しで結合された複数の募集タグの文字列から、募集タグを分割したリストを作成
+    '''
     result = []
     while s:
         match = next(word for word in all_tag if s.startswith(word))
