f504799db04943a1d73f410d1061d8f343911bda
nanae <desperado.kazune@gmail.com>
2024-08-21 16:56:38 +0900

    Add test cases

--- test_tag_to_operators_mapper.py
+++ test_tag_to_operators_mapper.py
@@ -13,6 +13,24 @@ class TestSplitStringByTags:
         expected_result = sorted(['近距離', '火力', '牽制'])
         assert split_string_by_tags(s, all_tag) == expected_result
 
+        s = '先鋒タイプ近距離COST回復支援'
+        expected_result = sorted(['先鋒タイプ', '近距離', 'COST回復', '支援'])
+        assert split_string_by_tags(s, all_tag) == expected_result
+
+        s = '前衛タイプ近距離火力支援'
+        expected_result = sorted(['前衛タイプ', '近距離', '火力', '支援'])
+        assert split_string_by_tags(s, all_tag) == expected_result
+
+    def test_separate_slash_case(self) -> None:
+        s = '近距離/火力/牽制'
+        expected_result = sorted(['近距離', '火力', '牽制'])
+        assert split_string_by_tags(s, all_tag) == expected_result
+
+    def test_noise_case(self) -> None:
+        s = '近距離,火力　牽制b'
+        expected_result = sorted(['近距離', '火力', '牽制'])
+        assert split_string_by_tags(s, all_tag) == expected_result
+
     def test_empty_case(self) -> None:
         s = ''
         expected_result: list[str] = []
