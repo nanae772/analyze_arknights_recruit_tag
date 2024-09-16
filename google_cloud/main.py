5c4a01afb85f446bc9641b029c554e8711389abf
nanae <desperado.kazune@gmail.com>
2024-09-16 10:19:31 +0900

    Add function send_content_to_discord_webhook

--- google_cloud/main.py
+++ google_cloud/main.py
@@ -1,4 +1,10 @@
+"""
+アークナイツの公開求人の画像をアップロードして、星４以上確定するタグの組合せをDiscordのWebhookに送る
+"""
 import base64
+import json
+import os
+import requests
 from google.cloud import vision
 from tag_to_operators_mapper import obtain_result_message, get_tag_list
 
@@ -21,7 +27,33 @@ def detect_text(request):
         print(tag_list)
 
         result_message = obtain_result_message(tag_list)
+        send_content_to_discord_webhook(result_message)
 
-        return result_message
+        return 'Success'
     else:
         return 'Error'
+
+
+class NotFoundDiscordWebhookUrl(Exception):
+    """環境変数からdiscordのwebhook urlが取得できなかったときのエラー"""
+
+
+def send_content_to_discord_webhook(content: str):
+    """ content を指定の discord webhook に送る"""
+    url = os.environ.get('DISCORD_WEBHOOK_URL')
+    if url is None:
+        raise NotFoundDiscordWebhookUrl('Cannot get DISCORD_WEBHOOK_URL')
+    message = {
+        "content": content,
+        "username": "Google Cloud Bot"
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(
+        url,
+        data=json.dumps(message),
+        headers=headers,
+        timeout=10
+    )
+    return f"Discord Webhook response: {response.status_code}"
